a356c07819394121566868acdba22e40ffdb8565
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-02 20:08:52 -0500

    Modificamos estilos del navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,41 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import logo from './react-logo.png';
+import '../styles/NavBar.css';
 
 class NavBar extends Component{
-
-
 
   render(){
     return(
       <div className="container-fluid">
-        <div className="row">
-          <nav className="navbar navbar-default navbar-fixed-side-left" role="navigation">
-            <div className="navbar-header">
-              <button type="button" className="pull-left navbar-toogle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span className="sr-only">Desplegar navegacion</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
+
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a className="navbar-brand " href="#">
+                <img className="navbar-logo" src={logo} alt="Logo" />
+              </a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="navbar-logobox">
-                <a href="/">
-                  <img src="" alt="Logo" />
-                </a>
+
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item navbar-listitem active ">
+                    <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                  </li>
+                  <li className="nav-item navbar-listitem">
+                    <Link className="nav-link" to="/bio">Bio</Link>
+                  </li>
+                  <li className="nav-item navbar-listitem">
+                    <Link className="nav-link" to="/description">Description</Link>
+                  </li>
+                  <li className="nav-item navbar-listitem">
+                    <Link className="nav-link disabled" to="/contact-us">Contact us</Link>
+                  </li>
+                </ul>
+
               </div>
-            </div>
-            <div className="navbar-collapse collapse">
-              <ul className="nav navbar-nav">
-                <li><a>Home</a></li>
-                <li><a>Bio</a></li>
-                <li><a>Description</a></li>
-                <li><a>Contact Us</a></li>
-              </ul>
-            </div>
-          </nav>
-        </div>
+            </nav>
+
       </div>
     );
   }
